7988a1d099084aafc11f37d7af5df21d5f4f6599
umer-shehzad <umer.work.96@gmail.com>
2024-04-29 19:31:00 +0500

    update product and create order schema

--- config/database.js
+++ config/database.js
@@ -1,19 +1,3 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', 'localhost'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'strapi-myshop'),
-//       user: env('DATABASE_USERNAME', 'postgres'),
-//       password: env('DATABASE_PASSWORD', 'Admin1122'),
-//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
-//       ssl: env.bool('DATABASE_SSL', false)
-//     },
-//     debug: false,
-//   },
-// });
-
 const path = require('path');
 
 module.exports = ({ env }) => {
