fa5a670e6bd1537ace78230c31cacac375f5fe80
umer-shehzad <umer.work.96@gmail.com>
2024-05-02 18:58:17 +0500

    add checkout form

--- frontend-nextjs/src/app/checkout/page.js
+++ frontend-nextjs/src/app/checkout/page.js
@@ -3,7 +3,52 @@ import React from 'react'
 const page = () => {
   return (
     <div className="container mx-auto px-4">
-      Checkout
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-24 mx-auto min-h-screen">
+          <div className="flex flex-col w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Checkout</h1>
+            <h2>Cart</h2>
+            <div className='cart'>Your cart details</div>
+          </div>
+          <div className="">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="fName" className="leading-7 text-sm text-gray-600">First Name</label>
+                  <input type="text" id="fname" name="fName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="lName" className="leading-7 text-sm text-gray-600">Last Name</label>
+                  <input type="text" id="lName" name="lName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email address</label>
+                  <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
+                  <input type="phone" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label htmlFor="message" className="leading-7 text-sm text-gray-600">Address</label>
+                  <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button className="flex text-white bg-indigo-500 border-0 py-1 md:py-2 md:px-8 px-4 focus:outline-none hover:bg-indigo-600 rounded md:text-lg text-sm">Pay Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
