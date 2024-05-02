3f633820c0a9d23aa088cdeff113d93a46a25a5e
umer-shehzad <umer.work.96@gmail.com>
2024-05-02 18:06:23 +0500

    add footer page

--- frontend-nextjs/src/app/layout.js
+++ frontend-nextjs/src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { CartProvider } from "@/components/CartContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <body className={inter.className}>
           <Navbar />
           {children}
+          <Footer />
         </body>
       </CartProvider>
     </html>
